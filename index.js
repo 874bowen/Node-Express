c1cb9caec6013dd192d01330c03b96c18f9e3ca8
Ivan Bowen <bowenivan16@gmail.com>
2022-10-07 16:36:23 +0300

    Express simple rest

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const express = require("express");
 const http = require("http");
-const morgan = require("morgan")
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
+app.all("/dishes", (req,res, next) => {
+     res.statusCode = 200;
+     res.setHeader("Content-Type", "text/plain");
+     next(); // continue to look for additional specs matching the /dishes endpoint
+});
+
+app.get("/dishes", (req, res, next) => {
+     res.end("Will send all the dishes to you");
+});
+
+app.post("/dishes", (req, res, next) => {
+     res.end("Will add the dish: " + req.body.name + " with details: "+ req.body.description);
+});
+app.put("/dishes", (req, res, next) => {
+     res.statusCode = 403
+     res.end("put operation not allowed on /dishes");
+});
+app.delete("/dishes", (req, res, next) => {
+     res.end("Deleting all the dishes!");
+});
+
+
+app.get("/dishes/:dishId", (req, res, next) => {
+     res.end("Will send the detailes of the dish " + req.params.dishId + "to you!");
+});
+
+app.post("/dishes/:dishId", (req, res, next) => {
+     res.statusCode = 403
+     res.end("POST operation not allowed on /dishes/" + req.params.dishId);
+});
+app.put("/dishes/:dishId", (req, res, next) => {
+     res.write("updating the dish: "+ req.params.dishId); // add a line to the reply message
+     res.end("Will update the dish: " + req.params.dishId + " with details: "+ req.body.description);
+});
+app.delete("/dishes/:dishId", (req, res, next) => {
+     res.end("deleting the dish: "+ req.params.dishId)
+});
 // serve up static files
 app.use(express.static(__dirname + '/public'));
 
